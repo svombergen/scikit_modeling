310739d955dac146b04b64156fd68b7b52aca4ac
Steven van Ombergen <s.vanombergen@vanlanschot.com>
2018-08-31 13:54:23 +0000

    added xgboost trials, TomekLinks

--- scikit modeling.py
+++ scikit modeling.py
@@ -1,9 +1,14 @@
+# Update pip and packages:
+# python -m pip install --upgrade pip
+# pip list --outdated --format=freeze | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip install -U
+
 # Import packages
 import numpy as np
 import pandas as pd
 import pyodbc
 import getpass
 import imblearn
+import matplotlib as plt
 
 # Connection details
 driver= '{ODBC Driver 13 for SQL Server}'
@@ -39,13 +44,17 @@ y = df['seventy_flag_in_four_weeks'] # target
 from sklearn.metrics import classification_report, confusion_matrix, roc_curve, auc
 from sklearn.preprocessing import StandardScaler 
 from sklearn.model_selection import train_test_split
+from imblearn.under_sampling import TomekLinks
 
-def trainModelWithResults(model, X, y,rd_state=None,autoscale=1):
+def trainModelWithResults(model, X, y,rd_state=None,autoscale=1,usetomeklinks=1):
     X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.25, stratify=y, random_state=rd_state) # stratify the split because we have unbalanced target
     if autoscale==1:
         scaler = StandardScaler().fit(X_train)
         X_train = scaler.transform(X_train)
         X_test = scaler.transform(X_test)
+    if usetomeklinks==1:
+        tl = TomekLinks(return_indices=False)
+        X_train, y_train = tl.fit_sample(X_train, y_train)
     mfitted = model.fit(X_train,y_train)
     predictions = mfitted.predict(X_test)
     print(confusion_matrix(y_test, predictions))
@@ -58,30 +67,58 @@ def printFeatureImportances(model, X):
 
 # Classification with scikit GradientBoostingClassifier
 from sklearn.ensemble import GradientBoostingClassifier
-gb = GradientBoostingClassifier(n_estimators=100,learning_rate=0.25, max_depth=5)
-trainModelWithResults(gb,X,y, autoscale=0)
+gb = GradientBoostingClassifier(n_estimators=300,learning_rate=0.25, max_depth=10)
+trainModelWithResults(gb,X,y, autoscale=1)
 
 # Classification with scikit RandomForestClassifier
 from sklearn.ensemble import RandomForestClassifier
 rf = RandomForestClassifier(n_estimators=100)
-trainModelWithResults(rf,X,y, autoscale=0)
+trainModelWithResults(rf,X,y, autoscale=1)
 
 # Neural net
 from sklearn.neural_network import MLPClassifier
 nncl = MLPClassifier(solver='lbfgs')
-trainModelWithResults(nncl,X,y, autoscale=0)
+trainModelWithResults(nncl,X,y, autoscale=1)
+
+# xgboost trials, seems no extra value as addition to sklearn GBC
+# Run cell
+#%%
+import xgboost as xgb
+# xgb = xgboost.DMatrix(df,label='seventy_flag_in_four_weeks')
+param = {'max_depth': 5, 'eta': 1, 'silent': 1, 'objective': 'binary:logistic','nthread':4,'eval_metric':'auc'}
+num_round = 10
+X_train, X_test, y_train, y_test = train_test_split(X,y, test_size=0.25, stratify=y) # stratify the split because we have unbalanced target
+dtrain = xgb.DMatrix(X_train, label=y_train)
+dtest = xgb.DMatrix(X_test, label=y_test)
+evallist = [(dtest, 'eval'), (dtrain, 'train')]
+bst = xgb.train(param,dtrain,num_round,evallist)
+xgb.plot_importance(bst)
+xgb.plot_tree(bst, num_trees=2)
+plt.pyplot.show()
 
 # GridSearchCV
-learning_rates = [0.25]                 #learning_rates = [0.05, 0.1, 0.25, 0.5, 0.75, 1]
-n_estimators = [150]                    #n_estimators = [25, 100, 250, 600]
-subsample = [1]                         #subsample = [0.1, 0.25, 0.5, 1]
-max_depth = [5]                        #max_depth = [1, 3, 5, 10]  
 from sklearn.model_selection import GridSearchCV
-parameters = {'learning_rate':[0.05, 0.1, 0.25, 0.5, 0.75, 1]}
-gcv = GridSearchCV(estimator = gb, param_grid=parameters, n_jobs=4, scoring='f1')
+parameters = {'learning_rate':[0.05, 0.1, 0.25, 0.5, 0.75, 1]
+    ,'max_depth':[1, 3, 5, 10]
+    ,'n_estimators':[25, 100, 500]}
+gcv = GridSearchCV(estimator = GradientBoostingClassifier(), param_grid=parameters, n_jobs=4, scoring='f1')
 gcv.fit(X,y)
-# Smote
-gcv.cv_results_
+trainModelWithResults(gcv.best_estimator_,X,y, autoscale=1)
+printFeatureImportances(gcv.best_estimator_,X)
+print(gcv.best_estimator_)
+
+# Smote alleen op je training, tomek links
+from imblearn.under_sampling import TomekLinks
+tl = TomekLinks(return_indices=False)
+X_resampled, y_resampled = tl.fit_sample(X_train, y_train)
+
+
+
+# Ensembling evt voting classifier
+
+
+
 
 # Threshold tweaking -> function with input target precision/recall/f1 and 
+# Categorical values -> onehotencoder
 
